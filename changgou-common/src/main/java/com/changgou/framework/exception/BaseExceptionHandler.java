0a9f9b53698225588de305de1eacc93885383824
jiangbaoping <1449499768@qq.com>
2020-03-26 16:51:15 +0800

    畅购商城第二天

---
+++ changgou-common/src/main/java/com/changgou/framework/exception/BaseExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.changgou.framework.exception;
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+//@ControllerAdvice注解，全局捕获异常类，只要作用在@RequestMapping上，所有的异常都会被捕获。
+public class BaseExceptionHandler {
+
+    /***
+     * 异常处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage());
+    }
+}
